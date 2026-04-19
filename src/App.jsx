608f8527e1413f06031f44dde4c6c0afc2404f57
RanjulaJMN <ranjulajmn41@gmail.com>
2026-04-20 00:35:29 +0530

    Initial Commit for lankan local trails frontend

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+     <div>
+      <h1>This is My Frontend for lankan local trials</h1>
+     </div>
+    </>
+  )
+}
+
+export default App
